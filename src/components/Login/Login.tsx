192d9a7e3cf58ef74b1f8daa8602915039cab16e
AnJuHwan <wnghks0531@gmail.com>
2022-04-03 15:24:32 +0900

    form

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+const Login: React.FC = (props) => {
+  return (
+    <div>
+      <p className={styles.signup}>회원가입</p>
+      <form className={styles.form}>
+        <label htmlFor='email'>이메일</label>
+        <input type='text' id='email' />
+
+        <label htmlFor='password'>비밀번호</label>
+        <input type='password' />
+        <label htmlFor='password'>비밀번호 확인</label>
+        <input type='password' />
+        <button>가입하기</button>
+        <button>로그인으로 가기</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
